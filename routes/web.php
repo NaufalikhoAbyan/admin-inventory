a441c892894aec57bf0f60bc7b33c98e3748b590
NaufalikhoAbyan <apradana448@gmail.com>
2023-12-07 14:39:55 +0700

    Barang Keluar CRUD complete

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\BarangMasukController;
+use App\Http\Controllers\BarangKeluarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,4 @@ Route::get('/dashboard', function(){
 Route::resource('kategori', KategoriController::class);
 Route::resource('barang', BarangController::class);
 Route::resource('barangmasuk', BarangMasukController::class);
+Route::resource('barangkeluar', BarangKeluarController::class);
